7c656cef7653f321f1e115c7e275a34afa7a4457
danielk98 <daniel.karl@student.uni-siegen.de>
2023-12-14 00:04:26 +0100

    add search logic and cli

---
+++ app/src/main/kotlin/Searcher.kt
@@ -0,0 +1,120 @@
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Path
+import java.util.*
+import kotlin.concurrent.thread
+import kotlin.io.path.Path
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.listDirectoryEntries
+import kotlin.math.max
+
+class Searcher {
+    var badCharacterTable: Map<Char, Int> = emptyMap<Char, Int>()
+    fun recursiveFileSearch(path: String, pattern: CharArray){
+        val paths: List<Path> = Path(path).listDirectoryEntries()
+        for (p in paths) {
+            if (Path(p.toString()).isRegularFile()) {
+                thread(start = true) {
+                    Path(p.toString())
+                    searchAllLines(p.toString(), pattern)
+                }
+            }
+            else if (Path(p.toString()).isDirectory()) {
+                recursiveFileSearch(p.toString(), pattern)
+            }
+        }
+    }
+    fun searchAllLines(filePath: String, pattern: CharArray){
+        badCharacterTable = createBadCharacterShiftTable(pattern)
+
+        val inputStream: InputStream = File(filePath).inputStream()
+        var lineCount = 1
+
+        val resultList: MutableList<String> = mutableListOf()
+
+        inputStream.bufferedReader().forEachLine {
+            val line = preprocess(it)
+            val isMatch = searchStringInText(pattern, line)
+
+            if (isMatch)
+            {
+                resultList.add(filePath + " L. " + lineCount + " " + it.toString() + "\n")
+            }
+            lineCount++
+        }
+        printResult(resultList)
+    }
+
+    /**This method creates the look-up table delta 1 (See paper).
+     * It contains the shift values for all characters of a search pattern.
+     * If we encounter a mismatch, we take the mismatching character from the text T
+     * (the text that we compare our pattern against) and look it up in the badCharacterShiftTable.
+     * The shift value for that specific character will tell us how far we can skip our pattern ahead.
+     * The shift value for characters that are not in the table is always the length of the pattern.
+     * */
+    fun createBadCharacterShiftTable(pattern: CharArray): MutableMap<Char, Int>
+    {
+        val badCharacterTable = mutableMapOf<Char, Int>()
+        for (i in pattern.indices){
+            if (!badCharacterTable.containsKey(pattern[i])){
+                badCharacterTable.put(pattern[i], max(1,pattern.size - i - 1))
+            }
+            else //overwrite shift value of char with the shift value of the char that occurs at a higher index
+            {
+                badCharacterTable[pattern[i]] = max(1, pattern.size - i - 1)
+            }
+        }
+        return badCharacterTable
+    }
+    fun printResult(result: MutableList<String>){
+        for (i in result.indices)
+            print(result[i])
+    }
+    //preprocess line as well as pattern into a sequence of chars
+    fun searchStringInText(pattern: CharArray, line: CharArray): Boolean
+    {
+        val patternLen = pattern.size
+        val lineLen = line.size
+        var patIndex = patternLen - 1 //we compare the pattern with the line from left to right, but start from the right side of the pattern to look for matches in the line
+        var lineIndex = patIndex
+        var shiftValue = 0
+        var shiftTotal = 0
+        var matchedChar = 0
+
+        //lineIndex < lineLen
+        while(lineLen - shiftTotal >= patternLen )
+        {
+            if(pattern[patIndex].equals(line[lineIndex]))
+            {
+                if (patIndex == 0)
+                    return true
+                ++matchedChar
+                --patIndex
+                --lineIndex
+            }
+            else
+            {
+                lineIndex += matchedChar
+                shiftValue = getShiftValue(line, lineIndex, patternLen)
+                lineIndex += shiftValue
+                shiftTotal += shiftValue
+                //reset index to start from the right again
+                patIndex = patternLen -1
+                matchedChar = 0
+            }
+        }
+        return false
+    }
+
+    fun getShiftValue(line: CharArray, lineIndex: Int, patternLen: Int): Int
+    {
+        return badCharacterTable[line[lineIndex]] ?: patternLen
+    }
+
+    fun preprocess(line: String): CharArray {
+        val result = line.lowercase(Locale.getDefault());
+        return result.toCharArray()
+    }
+
+}
---
+++ app/src/main/kotlin/kotlin/programming/languages/workshop/App.kt
@@ -0,0 +1,43 @@
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
+
+class Search : CliktCommand() {
+    //options
+    val pattern: String by argument(help = "substring to search for")
+    val path: String by argument(help = "file system path")
+
+    override fun run() {
+        val searcher = Searcher()
+        searcher.recursiveFileSearch(path, searcher.preprocess(pattern))
+    }
+}
+
+class AfterContextSearch : CliktCommand(){
+    //options
+    val afterContext: Int? by option("-A", "--after-context", help= "prints the given number of following lines for each match").int().default(1)
+    val pattern: String by argument(help = "substring to search for")
+    val path: String by argument(help = "file system path")
+
+
+    override fun run() {
+        println("AfterContextSearch")
+        val searcher = Searcher()
+        searcher.recursiveFileSearch(path, searcher.preprocess(pattern))
+    }
+}
+fun main(args: Array<String>) = Search()
+                                .main(args)
+/*
+val afterContext: Int? by option("-A", "--after-context", help= "prints the given number of following lines for each match").int().default(1)
+val beforeContext: Int? by option("-B", "--before-context", help= "prints the given number of preceding lines for each match").int().default(1)
+val context: Int? by option("-C", "--context", help= "prints the number of preceding and following lines for each match").int().default(1)
+val color by option("-c", "--color", help= "prints with colors, highlighting the matched phrase in the output").flag()
+val hidden by option("-h", "--hidden", help= "search hidden files and folders").flag()
+val help by option(help="prints this message").flag()
+val ignoreCase by option("-i", "--ignore-case", help= "search case insensitive").flag()
+val noHeading by option("--no-heading", help= "prints a single line including the filename for each match, instead of grouping matches by file").flag()
+//val exit: Int? by option(help = "exit program with 0").int().default(0)*/
